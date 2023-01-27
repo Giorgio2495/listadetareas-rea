f84ba946d4c07a4170fdc741af4a79ea6b0577ca
Giovani Mesa <giovaniitj@gmail.com>
2023-01-26 23:45:04 -0500

    end

--- src/components/ListaDeTareas.jsx
+++ src/components/ListaDeTareas.jsx
@@ -14,6 +14,21 @@ const ListaDeTareas = () => {
         }
     }
 
+    const eliminarTarea = id => {
+        const tareasActualizadas = tareas.filter(tarea => tarea.id!== id);
+        setTareas(tareasActualizadas);
+    }
+
+    const completarTarea = id => {
+        const tareasActualizadas = tareas.map(tarea => {
+            if(tarea.id === id) {
+                tarea.completada = !tarea.completada;
+            }
+            return tarea;
+        });
+        setTareas(tareasActualizadas);
+    }
+
 
   return (
     <>
@@ -26,12 +41,15 @@ const ListaDeTareas = () => {
         id={tarea.id}
         texto={tarea.texto}
         completada={tarea.completada}
+        completarTarea={completarTarea}
+        eliminarTarea={eliminarTarea}
+        
         />
         )
     }
      </div>
     </>
-  )
+  );
 }
 
 export default ListaDeTareas
--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -5,16 +5,18 @@ import { AiOutlineCloseCircle } from 'react-icons/ai'
 const Tarea = ({ id, texto, completada, completarTarea, eliminarTarea }) => {
   return (
     <div className={completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}>
-        <div className='tarea-texto'
-            onCLick={() => completarTarea(id)}>
-            {texto}
+        <div 
+        className='tarea-texto'
+        onCLick={() => completarTarea(id)}>
+        {texto}
         </div>
-        <div className='tarea-contenedor-iconos'
-        onClick={() => eliminarTarea(id)}>
-           <AiOutlineCloseCircle className='tarea-icono'/>
+        <div 
+          className='tarea-contenedor-iconos'
+          onClick={() => eliminarTarea(id)}>
+          <AiOutlineCloseCircle className='tarea-icono'/>
         </div>
     </div>
-  )
+  );
 }
 
-export default Tarea
+export default Tarea;
